c4c7d2ad352b47b148e4a48eadf2d95e4dc108ce
Iain Tooley <iaintooley1@gmail.com>
2019-09-28 16:58:58 +0100

    added a way through tickets to see the films of a customer and vice versa

--- weekend/models/tickets.rb
+++ weekend/models/tickets.rb
@@ -16,24 +16,40 @@ class Ticket
     customer_id,
     film_id)
     VALUES (
-    $1, $2)
-    RETURNING id"
-    values = [@customer_id, @film_id]
-    ticket = SqlRunner.run(sql, values).first
-    @id = ticket['id'].to_i
+      $1, $2)
+      RETURNING id"
+      values = [@customer_id, @film_id]
+      ticket = SqlRunner.run(sql, values).first
+      @id = ticket['id'].to_i
+    end
+
+    def customer()
+      sql = "SELECT * FROM customers
+      WHERE id = $1"
+      values = [@customer_id]
+      customer_hash = SqlRunner.run(sql, values)[0]
+      return Customer.new(customer_hash)
+    end
+
+    def film()
+      sql = "SELECT * FROM films
+      WHERE id = $1"
+      values = [@film_id]
+      film_hash = SqlRunner.run(sql, values).first()
+      return Film.new(film_hash)
+    end
+
+    def self.delete_all
+      sql = "DELETE FROM tickets"
+      SqlRunner.run(sql)
+    end
+
+    def self.all
+      sql = "SELECT * FROM tickets"
+      results = SqlRunner.run(sql)
+      tickets = results.map { |tickets_hash| Ticket.new(tickets_hash)}
+      return tickets
+    end
+
+
   end
-
-  def self.delete_all
-    sql = "DELETE FROM tickets"
-    SqlRunner.run(sql)
-  end
-
-  def self.all
-    sql = "SELECT * FROM tickets"
-    results = SqlRunner.run(sql)
-    tickets = results.map { |tickets_hash| Ticket.new(tickets_hash)}
-    return tickets
-  end
-
-
-end
